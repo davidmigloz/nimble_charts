f5eb0c34bd73dd0d002d13f232ef49051257d85d
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-12-13 11:39:25 +1100

    spellinz

--- charts_flutter/test/example_widget_tests/example_test.dart
+++ charts_flutter/test/example_widget_tests/example_test.dart
@@ -635,7 +635,7 @@ extension ExampleWidgetTestExtensions on WidgetTester {
       },
     );
 
-    //TODO: figure out who draguntilvisible is not working
+    //TODO: figure out who drag until visible is not working
     //It never seems to find the tile... Specifying the scrollDelta is a
     //workaround
     // Scroll to the button if needed.
